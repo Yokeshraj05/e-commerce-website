2f58d6c715059a10f45dcfaba0408ace83c03081
Yokeshraj05 <yokeshraj6662@gmail.com>
2024-07-25 10:49:46 +0530

    web

---
+++ scripts/topics.js
@@ -0,0 +1,21 @@
+var head=document.getElementById('head');
+var html=document.getElementById('html');
+
+html.onclick=function() {
+    head.style.display="block";
+}
+
+var cs=document.getElementById('cs');
+var css=document.getElementById('css');
+
+css.onclick=function() {
+    cs.style.display="block";
+}
+
+
+var jsc=document.getElementById('jsc');
+var js=document.getElementById('js');
+
+js.onclick=function() {
+    jsc.style.display="block";
+}
